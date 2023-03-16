9269c32ee512b48b06d94e0c8663a4b900ef04cf
sterling_hebert <sterling_hebert@outlook.com>
2023-03-16 17:32:25 -0500

    phase 1 and setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store";
+import "./index.css";
+import App from "./App";
+
+const store = configureStore();
 
 function Root() {
+  if (process.env.NODE_ENV !== "production") {
+    window.store = store;
+  }
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   );
 }
 
@@ -16,5 +25,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
